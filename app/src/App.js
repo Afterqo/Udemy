a3d5a36604a2df434491a4d300d04ab46f6c6b82
Jakub Kytka <afterqo@gmail.com>
2023-11-16 19:31:42 +0100

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Welcome to my app</h1>
+        </div>
+      </header>
+      <body className='App-body'>
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
